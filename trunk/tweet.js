5ca74faa6ad3d591151492e8f1c249d6db73d49c
louleboule <louleboule@aba40ba9-5a8c-4cf7-b5c2-f073e062adfa>
2018-11-30 19:01:40 +0000

    Ajout du parser, des données, et du fichier qui renvoie une liste de tous les Tweets

---
+++ trunk/tweet.js
@@ -0,0 +1,39 @@
+var Papa = require('./parser.js');
+let fs = require('fs');
+
+let Alltweets = [];
+let pathOfTweets = ["donneesSujetA/Fri Mar 23/tweets.csv", 
+					"donneesSujetA/Mon Mar 19/tweets.csv",
+					"donneesSujetA/Mon Mar 26/tweets.csv",
+					"donneesSujetA/Sat Mar 17/tweets.csv",
+					"donneesSujetA/Sat Mar 24/tweets.csv",
+					"donneesSujetA/Sun Mar 18/tweets.csv",
+					"donneesSujetA/Thu Mar 22/tweets.csv",
+					"donneesSujetA/Tue Mar 20/tweets.csv",
+					"donneesSujetA/Wed Mar 21/tweets.csv"];
+
+
+//Sépare tous les Tweets d'un fichier et les met dans le tableau Alltweets
+function parseTweets(fichierDeTweet){
+	var results = Papa.parse(fs.readFileSync(fichierDeTweet,'utf8'), {
+		header: true,
+		complete: function(results) {
+			tweets = results.data;
+
+			for(var i = 0; i<tweets.length; i++){
+				Alltweets.push(tweets[i]);
+			}
+		}
+	});
+}
+
+//Itère sur tous les fichiers de Tweet déclarés dans la variable pathOfTweets
+function getAllTweets(){
+	for(var j = 0; j<pathOfTweets.length; j++){
+		parseTweets(pathOfTweets[j]);
+	}
+	return Alltweets;
+}
+
+
+module.exports.getAllTweets = getAllTweets;
