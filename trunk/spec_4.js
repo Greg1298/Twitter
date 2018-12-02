7cafcd083d148f1ca1d8ed7ae1f1e5366a7c67b7
louleboule <louleboule@aba40ba9-5a8c-4cf7-b5c2-f073e062adfa>
2018-12-02 15:38:56 +0000

    MAJ pour Caporal, Ajout Spec 5

--- trunk/spec_4.js
+++ trunk/spec_4.js
@@ -1,69 +1,80 @@
 const Tweets = require("./tweet.js");
+const program = require('caporal');
 
-let hashtag = "mpgvip";
+exports.module = require('caporal')
 
-let TousleshashTags = [];
+	.command("getRelatedHashtags", "Retourne une liste de HashTags ayant été liés au HashTag recherché.")
+	.argument("<hashtag>", "Entrez un HashTag dont vous cherchez les relations.")
 
-allTweets = Tweets.getAllTweets();
+	.action(function (args, options, logger){
+		console.log("Les HashTags associés au HashTag \"" + args.hashtag + "\" sont :");
+		console.log(getRelatedHashtags(args.hashtag));
+	})
 
-for(var k = 0; k < allTweets.length; k++){
+program.parse(process.argv);
 
-	//On récupère les HashTags d'un Tweet dans une variable
-	ht = allTweets[k].hashtags;
+function getRelatedHashtags(tag) {
 
-	//On sépare tous les HashTags et les range dans un tableau
-	tabHashTagsduntweet = ht.split(" ");
+	allTweets = Tweets.getAllTweets();
+	let TousleshashTags = [];
 
-	contain = false;
+	for(var k = 0; k < allTweets.length; k++){
 
-	//On réalise l'opération pour tous les HashTags d'un Tweet
-	tabHashTagsduntweet.forEach(function(unhashtagduntweet) {
-		//On vérifie si le HashTag étudié est le HashTag dont on cherche les relations
-		if(hashtag.toLowerCase().indexOf(unhashtagduntweet.toLowerCase()) != -1){
-			//On vérifie que ce n'est pas une chaine de caractère vide
-			if(unhashtagduntweet != ""){
+		//On récupère les HashTags d'un Tweet dans une variable
+		ht = allTweets[k].hashtags;
 
-				//Cela veut dire que ce Tweet comporte le HashTag dont on cherche les relations
-				contain = true;
+		//On sépare tous les HashTags et les range dans un tableau
+		tabHashTagsduntweet = ht.split(" ");
 
-			}
-			else{
-				contain = false;
-			}
-		}
-	});
+		contain = false;
 
-	//Si les HashTags du Tweet étudiés comportent le HashTag dont on cherche les relations
-	if(contain == true){
-
-		//On réalise l'opération pour tous les HashTags du Tweet
+		//On réalise l'opération pour tous les HashTags d'un Tweet
 		tabHashTagsduntweet.forEach(function(unhashtagduntweet) {
+			//On vérifie si le HashTag étudié est le HashTag dont on cherche les relations
+			if(tag.toLowerCase().indexOf(unhashtagduntweet.toLowerCase()) != -1){
+				//On vérifie que ce n'est pas une chaine de caractère vide
+				if(unhashtagduntweet != ""){
 
-			//On effectue les opérations qui suivent uniquement si ce n'est pas le HashTag dont on cherche les relations
-			if(hashtag.toLowerCase().indexOf(unhashtagduntweet.toLowerCase()) == -1){
+					//Cela veut dire que ce Tweet comporte le HashTag dont on cherche les relations
+					contain = true;
 
-				contain = false;
-
-				//On vérifie que le HashTag n'est pas déjà présent dans le tableau TousleshashTags
-				TousleshashTags.forEach(function(entree){
-					//Si on trouve une correspondance
-					if(entree.toLowerCase().indexOf(unhashtagduntweet.toLowerCase()) != -1){
-
-						//On modifie la variable pour dire que le HashTag existe déjà dans le tableau, on ne le rajoutera donc pas une nouvelle fois
-						contain = true;
-
-					}
-				});
-
-				//Si on n'a pas trouvé de correspondance on ajoute le HashTag étudié à la liste des HashTags associés au HashTag dont on cherche les relations
-				if(contain == false){
-			  		TousleshashTags.push(unhashtagduntweet);
 				}
-
+				else{
+					contain = false;
+				}
 			}
 		});
-	}
-}
 
-console.log("Les HashTags associés au HashTag \"" + hashtag + "\" sont :");
-console.log(TousleshashTags);
+		//Si les HashTags du Tweet étudiés comportent le HashTag dont on cherche les relations
+		if(contain == true){
+
+			//On réalise l'opération pour tous les HashTags du Tweet
+			tabHashTagsduntweet.forEach(function(unhashtagduntweet) {
+
+				//On effectue les opérations qui suivent uniquement si ce n'est pas le HashTag dont on cherche les relations
+				if(tag.toLowerCase().indexOf(unhashtagduntweet.toLowerCase()) == -1){
+
+					contain = false;
+
+					//On vérifie que le HashTag n'est pas déjà présent dans le tableau TousleshashTags
+					TousleshashTags.forEach(function(entree){
+						//Si on trouve une correspondance
+						if(entree.toLowerCase().indexOf(unhashtagduntweet.toLowerCase()) != -1){
+
+							//On modifie la variable pour dire que le HashTag existe déjà dans le tableau, on ne le rajoutera donc pas une nouvelle fois
+							contain = true;
+
+						}
+					});
+
+					//Si on n'a pas trouvé de correspondance on ajoute le HashTag étudié à la liste des HashTags associés au HashTag dont on cherche les relations
+					if(contain == false){
+						TousleshashTags.push(unhashtagduntweet);
+					}
+
+				}
+			});
+		}
+	}
+	return TousleshashTags;
+}
---
+++ trunk/spec_5.js
@@ -0,0 +1,29 @@
+const Tweets = require("./tweet.js");
+const program = require('caporal');
+
+exports.module = require('caporal')
+
+	.command("getLocatedTweets", "Retourne le nombre de Tweets ayant été postés depuis la localisation saisie.")
+	.argument("<location>", "Entrez une localisation pour connaître le nombre de Tweets postés depuis celle-ci.")
+
+	.action(function (args, options, logger){
+		console.log(getLocatedTweets(args.location) + " tweets ont été postés depuis \"" + args.location + "\"");
+	})
+
+program.parse(process.argv);
+
+
+function getLocatedTweets(location){
+    let nbtweets = 0;
+    allTweets = Tweets.getAllTweets();
+    allTweets.forEach(tweet => {
+        loc = tweet.user_location;
+        sp = loc.split(", ");
+        sp.forEach(element => {
+            if(element.toLowerCase().indexOf(location.toLowerCase()) != -1){
+                nbtweets++;
+            }
+        });
+    });
+    return nbtweets;
+}
