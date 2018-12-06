114ee70cca3034b12f6828bc406fca18d78713ad
tourem <tourem@aba40ba9-5a8c-4cf7-b5c2-f073e062adfa>
2018-12-06 20:59:27 +0000

    Ajout du composant TweetAnalyzer.js dans trunk

--- trunk/TweetAnalyzer.js
+++ trunk/TweetAnalyzer.js
@@ -1,10 +1,14 @@
 const spec_1 = require("./spec_1.js");
 const SPEC_2 = require("./spec_2.js");
+const spec_2 = require('./spec_2.js');
 const SPEC_3 = require("./spec_3.js");
+const spec_3 = require('./spec_3.js');
 const spec_4 = require("./spec_4.js");
 const spec_5 = require("./spec_5.js");
 const program = require('caporal');
 const fs = require('fs');
+const Tweets = require("./tweet.js");
+let stresult = "";
 
 exports.module = require('caporal')
 
@@ -35,7 +39,7 @@ exports.module = require('caporal')
 			stresult = stresult.concat(",");
 		});
 
-		fs.writeFile('10TweetPopulaireAvecTagPopulaire.txt', stresult, function (err) {
+		fs.writeFile('10TweetPopulaireAvecTagPopulaire.html', stresult, function (err) {
 			if (err) throw err;
 			console.log("Un fichier comportant le résultat de la requête a été généré !");
 		});
@@ -93,4 +97,27 @@ exports.module = require('caporal')
 		console.log(spec_5.getLocatedTweets(args.location) + " tweets ont été postés depuis \"" + args.location + "\"");
 	})
 
+	//SPEC_3
+	.command("visualizeAuteurPopAvecTagPop", "Visualise le top 10 des auteurs de tweets comportant le hashtag ayant été le plus retweeté")
+	.action(function (args, options, logger){
+		//SPEC_9
+	stresult = stresult.concat("<!DOCTYPE html><head><title>Visualisation tweets</title><meta charset=\"utf-8\"><script src=\"https://cdn.jsdelivr.net/npm/vega@4.3.0/build/vega.js\"></script><script src=\"https://cdn.jsdelivr.net/npm/vega-lite@3.0.0-rc10/build/vega-lite.js\"></script><script src=\"https://cdn.jsdelivr.net/npm/vega-embed@3.24.1/build/vega-embed.js\"></script><style media=\"screen\">.vega-actions a {margin-right: 5px;}</style></head><body><h1>La visualisation des 10 auteurs les plus influents</h1><div id=\"vis\"></div><script>var vlSpec = {\"$schema\": \"https://vega.github.io/schema/vega-lite/v3.json\",\"data\": {\"values\": [");
+
+	rs = spec_3.getAuteurPopulaireAvecTagPopulaire(spec_2.TagPlusPopulaire());
+
+	rs.forEach(function(value, key)  {
+	    stresult = stresult.concat("{\"a\": \"" + key + "\", \"b\": " + value + "},");
+	});
+
+	stresult = stresult.substring(0,stresult.length-1);
+	stresult = stresult.concat("]},\"mark\": \"bar\",\"encoding\": {\"x\": {\"field\": \"a\",\"type\": \"nominal\",\"axis\": {\"title\": \"Auteurs influents\"}},\"y\": {\"aggregate\": \"average\",\"field\": \"b\",\"type\": \"quantitative\",\"axis\": {\"title\": \"Nombre de tweets\"}}},\"config\": {\"axisY\": {\"minExtent\": 30}}};vegaEmbed(\"#vis\", vlSpec);</script></body></html>");
+
+	fs.writeFile('visuTweets.html', stresult, function (err) {
+	    if (err) throw err;
+	    console.log("Une page HTML comportant le résultat de la requête a été générée !");
+	})
+		
+	});
+
+	
 program.parse(process.argv);
